4816f4c55d127f1eb6b0443d8ef5bf893a14f324
raspaev.niko <raspaev.niko@gmail.com>
2024-12-04 21:01:12 +0300

    MP-2

--- src/main/java/com/cpo/med/configuration/MinioConfig.java
+++ src/main/java/com/cpo/med/configuration/MinioConfig.java
@@ -1,6 +1,8 @@
 package com.cpo.med.configuration;
 
 import com.cpo.med.configuration.properties.MinioProperties;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +18,17 @@ public class MinioConfig {
     @Bean
     public MinioClient minioClient() {
         try {
-            return MinioClient.builder()
+            MinioClient minioClient = MinioClient.builder()
                     .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
                     .endpoint(minioProperties.getEndpoint())
                     .build();
+
+            String bucketName = minioProperties.getBucketName();
+            if (!minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build())) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+
+            return minioClient;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage(), e);
         }
